f819bb9826a44a4c6d498a974809d6af31849096
tamimattafi <tattafi@zennex.ru>
2020-03-30 09:47:23 +0700

    Refactoring

--- src/main/kotlin/com/tamimattafi/backend/demo/api/controllers/home/HomeResponses.kt
+++ src/main/kotlin/com/tamimattafi/backend/demo/api/controllers/home/HomeResponses.kt
@@ -6,11 +6,11 @@ object HomeResponses {
 
     const val ERROR_NOT_FOUND = "Page Not Found"
 
-    const val RESPONSE_WELCOME = "Welcome to Kotlin Spring Boot demo app!" +
-            "\n This app was designed for educational purposes in order to learn and teach!" +
-            "\n This app can be used by both back-end, or even by front-end developers to learn to implement REST Api clients" +
-            "\n More information could be found on our github repo: https://github.com/tamimattafi/backend-demo" +
-            "\n Happy Coding!"
+    const val RESPONSE_WELCOME = "<h1>Welcome to Kotlin Spring Boot demo app!</h1>" +
+            "\n<p> This app was designed for educational purposes in order to learn and teach!" +
+            "\n This app can be used by both back-end, or even by front-end developers to learn to implement REST Api clients!</p>" +
+            "\n<h2> More information could be found on our github repo: <b>https://github.com/tamimattafi/backend-demo</b></h2>" +
+            "\n<h1> Happy Coding!<h1>"
 
     val ERROR_PAGE_NOT_FOUND
         get() = ErrorResponse(ERROR_NOT_FOUND, "This page cannot be found. Please check our repository at https://github.com/tamimattafi/backend-demo for more information")
